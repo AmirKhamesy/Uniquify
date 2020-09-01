b779846482bfe6e8060ca7ca51c33fd69468d684
partrima <iknubs@gmail.com>
2020-09-01 16:13:52 -0700

    heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 var SpotifyWebApi = require('spotify-web-api-node');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const path = require('path');
 const { response } = require('express');
 const { log } = require('console');
