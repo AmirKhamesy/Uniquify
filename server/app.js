e3938e8ff8e657dea60854e69c10f998d263bba2
partrima <iknubs@gmail.com>
2021-02-17 21:40:01 -0800

    working on stlying

--- server/app.js
+++ server/app.js
@@ -77,7 +77,7 @@ const getSongs = async (playlistID) => {
     }
     console.log(songs)
 
-    return songs.map(song => "" + song.track.name + "\n" + song.track.album.artists[0]["name"]);
+    return songs.map(song => {return "%s\n%s" , song.track.name , song.track.album.artists[0]["name"]});
 }
 
 const getPlaylistName = async (playlistID) => {
