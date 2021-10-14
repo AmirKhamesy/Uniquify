679eb2c3a6314b1d97c388bc9a688e59e1d2684c
Amir <iknubs@gmail.com>
2021-10-13 20:34:44 -0700

    Removing old packages

--- uniquify/src/index.js
+++ uniquify/src/index.js
@@ -2,42 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
 import Home from './Pages/Home';
-import { Link } from 'react-router-dom';
-import { Box, Text, Image } from 'grommet'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { StyleSheetManager } from 'styled-components';
 
 AOS.init()
 ReactDOM.render(
 
   <React.StrictMode>
     <Home />
-    {/* <Router>
-      <Box direction="row" justify='between' margin={{ right: 'auto' }}>
-        <Box direction='column'>
-          <Link to="/" style={styles.link}>
-            <Box direction='row-responsive' className="typewriter">
-              <h1>Uniquify</h1>
-            </Box>
-          </Link>
-        </Box>
-        <Box direction='column' >
-          <Link to="/compare" style={styles.link}>
-            <Box
-              height="xsmall"
-              round='medium'
-              elevation='xlarge'
-              width="xsmall"
-              overflow='hidden'>
-              <Image src="https://cdn.shopify.com/app-store/listing_images/47509b0ae6effab2437add3d7c7dcfa9/icon/CNr7nL30lu8CEAE=.png"></Image>
-            </Box>
-          </Link>
-        </Box>
-      </Box>
-      <Route path='/' exact component={LandingPage}></Route>
-      <Route path='/compare' exact component={Home}></Route>
-    </Router> */}
+
   </React.StrictMode >,
   document.getElementById('root')
 );
